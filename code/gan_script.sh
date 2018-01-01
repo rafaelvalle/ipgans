cb0229ee8364070ed7b65a83d3e0652b2f758897
Rafael Valle <jrafaelvalle@gmail.com>
2018-01-01 15:10:58 -0800

    adding scripts for running extensive experiments and creating gif files

---
+++ code/gan_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+# declare gans to be used
+declare -a gans=("dcgan") # gans=("dcgan" "lsgan" "wgan" "wgan-gp")
+declare -a thresholds=("0.5" "0.0")
+declare -a activations=("sigmoid" "scaled_tanh" "linear")
+declare -a optimizers=("adam" "rmsprop") # ("adam" "rmsprop")
+
+
+# loop over gans and run gans
+for gan in "${gans[@]}"; do
+  for threshold in "${thresholds[@]}"; do
+    for activation in "${activations[@]}"; do
+      for optimizer in "${optimizers[@]}"; do
+        if [ "$gan" == "wgan-gp" ]; then
+          ./gan_mnist.py "$gan" --optimizer $optimizer --threshold ${threshold} --activation $activation --eta_decay 
+          ./gan_mnist.py "$gan" --optimizer $optimizer --threshold ${threshold} --activation $activation
+        else
+          ./gan_mnist.py "$gan" --optimizer $optimizer --do_batch_norm --threshold ${threshold} --activation $activation --eta_decay 
+          ./gan_mnist.py "$gan" --optimizer $optimizer --do_batch_norm --threshold ${threshold} --activation $activation
+        fi
+      done
+    done
+  done
+done
---
+++ code/gif_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+folder_path=$1
+declare -a gans=("dcgan" "lsgan" "wgan" "wgan-gp")
+declare -a bns=("True" "False")
+declare -a etas=("True" "False")
+declare -a opts=("adam" "rmsprop")
+declare -a acts=("sigmoid" "scaled_tanh" "linear")
+declare -a threshs=("0.0" "0.5")
+
+for gan in "${gans[@]}"; do
+  for bn in "${bns[@]}"; do
+    for eta in "${etas[@]}"; do
+      for opt in "${opts[@]}"; do
+        for act in "${acts[@]}"; do
+          for thresh in "${threshs[@]}"; do
+            if [ -e ${folder_path}${gan}_mnist_hist_epoch_0_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}.png ]; then 
+              echo ${folder_path}${gan}_mnist_hist_epoch_0_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}.png "does exist"
+              convert \
+                -resize 50% -delay 10 -loop 0 \
+                $(for i in $(seq 0 1 99); do 
+                  echo ${folder_path}${gan}_mnist_hist_epoch_${i}_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}.png; done) \
+                ${folder_path}${gan}_mnist_hist_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}.gif
+            else
+              echo ${folder_path}${gan}_mnist_hist_epoch_0_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}.png does not exist
+            fi
+          done
+        done
+      done
+    done
+  done
+done
