ae627f6d9f86b10bb360eeafc8256ae55cc74072
Rafael Valle <jrafaelvalle@gmail.com>
2018-01-15 16:01:04 -0800

    adding noise type to script

--- code/gif_script.sh
+++ code/gif_script.sh
@@ -7,6 +7,7 @@ declare -a etas=("True" "False")
 declare -a opts=("adam" "rmsprop")
 declare -a acts=("sigmoid" "scaled_tanh" "linear")
 declare -a threshs=("0.0" "0.5")
+declare -a noises=("uniform" "normal")
 
 for gan in "${gans[@]}"; do
   for bn in "${bns[@]}"; do
@@ -14,16 +15,18 @@ for gan in "${gans[@]}"; do
       for opt in "${opts[@]}"; do
         for act in "${acts[@]}"; do
           for thresh in "${threshs[@]}"; do
-            if [ -e ${folder_path}${gan}_mnist_hist_epoch_0_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}.png ]; then 
-              echo ${folder_path}${gan}_mnist_hist_epoch_0_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}.png found
-              convert \
-                -resize 50% -delay 10 -loop 0 \
-                $(for i in $(seq 0 1 199); do 
-                  echo ${folder_path}${gan}_mnist_hist_epoch_${i}_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}.png; done) \
-                ${folder_path}${gan}_mnist_hist_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}.gif
-            else
-              echo ${folder_path}${gan}_mnist_hist_epoch_0_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}.png missing 
-            fi
+            for noise in "${noises[@]}"; do
+              if [ -e ${folder_path}${gan}_mnist_hist_epoch_0_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}_noise_${noise}.png ]; then 
+                echo ${folder_path}${gan}_mnist_hist_epoch_0_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}_noise_${noise}.png found
+                convert \
+                  -resize 50% -delay 10 -loop 0 \
+                  $(for i in $(seq 0 1 199); do 
+                    echo ${folder_path}${gan}_mnist_hist_epoch_${i}_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}_noise_${noise}.png; done) \
+                  ${folder_path}${gan}_mnist_hist_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}_noise_${noise}.gif
+              else
+                echo ${folder_path}${gan}_mnist_hist_epoch_0_non_lin_${act}_opt_${opt}_bn_${bn}_etadecay_${eta}_thresh_${thresh}_noise_${noise}.png missing 
+              fi
+            done
           done
         done
       done
